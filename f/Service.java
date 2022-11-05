99428de21042b2d40d9922f4ebdb0cdb5f733bd7
MagicianK <45946407+MagicianK@users.noreply.github.com>
2022-11-05 23:47:37 +0600

    Add files via upload

---
+++ f/Service.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Service extends Remote {
+    Integer getMessage() throws RemoteException;
+    void sendMessage(int num) throws RemoteException;
+    void storeCalculatedMessage(int num) throws RemoteException;
+}
